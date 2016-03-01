559140a6bb94e42caa1723e9c9c53a21ee914a34
David Cramb <david.cramb@gmail.com>
2016-02-29 21:21:31 -0600

    palindrome function working

--- strings.js
+++ strings.js
@@ -1,6 +1,6 @@
 // //Copy the code below into the JavaScript file
-// In your HTML, create an text input and a button.
-// The text input should only accept letters. No numbers.
+// In your HTML, create an text input and a button. - done
+// The text input should only accept letters. No numbers. - done
 // Implement the logic in the reversal function that reverses the order of the characters in the string, and outputs the result in the DOM, below the text input.
 // Implement the logic in the alphabits function that return the characters in alphabetical order, and outputs the result in the DOM, below the text input.
 // Implement the logic in the palindrome function that determine whether the string is a palindrome. If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
@@ -8,10 +8,10 @@
 // The output of each of the functions should immediately appear as well.
 
 var button = document.getElementById('btn');
-var input = document.getElementById('input')
+var input = document.getElementById('input');
+var outputEl = document.getElementById('output');
 
 function reversal() {
-  var outputEl = document.getElementById('output');
   var toReverse = input.value.split("").reverse().join("");
   outputEl.innerHTML += toReverse;
   
@@ -20,16 +20,16 @@ function reversal() {
 function alphabits() {
   var alphabetical = input.value.split("").sort().join("");
   outputEl.innerHTML += alphabetical;
-}
+  };
 
 function palindrome() {
-  var outputEl = document.getElementById('output')
-  var racecar = input.value.toLowerCase().split("");
-  racecar.forEach(function
-    (char) {
-      console.log(racecar.indexOf(char))
-      })
-  
+  var uInput = input.value.toLowerCase();
+  var racecar = uInput.split("").reverse().join("");
+    if (uInput === racecar) {
+      outputEl.innerHTML += "This is a palindrome!" + "<br>";
+    } else {
+      outputEl.innerHTML += "No palindromes here." + "<br>"
+    };        
 }
 
 var testString = "";
@@ -38,8 +38,7 @@ var testString = "";
 // alphabits(testString);
 // palindrome(testString);
 
-function test() {
-  console.log(input.value)
-}
 
-button.addEventListener('click', palindrome);
+button.addEventListener('click', function(){
+  
+});
